a17af8bcb1ed91a1fe968b9cc1d89352986d3962
bcroman <bencollins315@gmail.com>
2024-11-30 13:28:58 +0000

    Check Permissions
    Created a function to check for permissions
    Start on the camera functions and layout

--- app/src/main/java/com/example/photodiary/CameraActivity.kt
+++ app/src/main/java/com/example/photodiary/CameraActivity.kt
@@ -1,25 +1,80 @@
 package com.example.photodiary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.photodiary.databinding.ActivityMainBinding
+import com.example.photodiary.databinding.ActivityCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding //Set ViewBinding Variable
+    private lateinit var ViewBinding: ActivityCameraBinding //Set ViewBinding Variable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        val ViewBinding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(ViewBinding.root)
+
+        //Capture Button to take Photo
+        ViewBinding.fabCapture.setOnClickListener { takePhoto() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        //Call Start Camera
+        startCamera()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    //Function to Take Photo
+    private fun takePhoto(){
+    }
+    //Function to open the Camera
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(ViewBinding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e("TAG", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+    // Function to shut down the Camera
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
