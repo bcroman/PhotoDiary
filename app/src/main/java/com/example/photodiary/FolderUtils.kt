f2a5285813f8d234258f66f6afc3a1ae1083bbfe
bcroman <bencollins315@gmail.com>
2024-12-14 19:50:55 +0000

    Folder Utils Class V1
    Setup Folder Utils Class
    Will need to add functions & test it

---
+++ app/src/main/java/com/example/photodiary/FolderUtils.kt
@@ -0,0 +1,12 @@
+package com.example.photodiary
+
+/*
+Folder Utils Class
+Handles the function to create a new folder 'PhotoDiary'
+Handles the function to save a img (png) to the 'PhotoDiary' folder
+Handles the function to delete the 'PhotoDiary' folder and all data in the folder
+This should work Android 10+ and some older versions
+ */
+
+class FolderUtils {
+}
