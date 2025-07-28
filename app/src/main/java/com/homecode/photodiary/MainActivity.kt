37cd191381d4322854746be58466db40ae9f7dbb
Ben C <bencollins315@gmail.com>
2025-07-28 12:09:55 +0100

    Feature: Check Permissions Function
    Created a function to request for all permissions for the user to allow or deny.
    This function will be run on runtime

--- app/src/main/java/com/homecode/photodiary/MainActivity.kt
+++ app/src/main/java/com/homecode/photodiary/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.homecode.photodiary
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.homecode.photodiary.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding //ViewBinding Variable
+const val PERMISSION_REQUEST_CODE: Int = 101 //Set Permission Request Code
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Call CheckPermission Function
+        checkPermissions()
+    }
+
+    //Function: Check for Permission on RunTime
+    private fun checkPermissions() {
+        // Define the permissions that you want to request (Copy from Manifest File)
+        val permissions = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        )
+
+        var allGranted = true // Flag to track if all permissions are already granted
+
+        // Loop through each permission and check if it is granted
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                allGranted = false // If any permission is not granted, set the flag to false
+                break // No need to check further, break the loop
+            }
+        }
+
+        if (!allGranted) {
+            // If at least one permission is not granted, request all the permissions
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE)
+        } else {
+            // If all permissions are already granted, notify the user
+            Toast.makeText(this, "All permissions are already granted!", Toast.LENGTH_SHORT).show()
         }
     }
 }
