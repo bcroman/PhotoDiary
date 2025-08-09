8901bda038f3b8ce97c009f942af55ddb6238e93
Ben C <bencollins315@gmail.com>
2025-08-09 13:01:42 +0100

    Core: Enable ViewBinding
    Add Viewbinding to Gradle
    Set main layout to Viewbinding

--- app/src/main/java/com/homecode/photodiary/MainActivity.kt
+++ app/src/main/java/com/homecode/photodiary/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.homecode.photodiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vBinding: ActivityMainBinding //Set ViewBinding Variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        vBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
